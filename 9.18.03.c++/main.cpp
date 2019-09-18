e3f8003811895ba27fd073a8909b25b5ca014028
20181105879 <2605753145@qq.com>
2019-09-18 10:59:30 +0800

    Initial Commit

---
+++ 9.18.03.c++/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  9.18.03.c++
+//
+//  Created by s20181105879 on 2019/9/18.
+//  Copyright © 2019 s20181105879. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
